ef166b85650617901cfef8d81144f7057ce80749
MaueDev <mauricio.rsc.mg@gmail.com>
2024-04-16 19:29:29 -0300

    feat: Iniciando Projeto

---
+++ src/main/java/com/likedinscrapping/competencia/CompetenciaApplication.java
@@ -0,0 +1,13 @@
+package com.likedinscrapping.competencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likedinscrapping/competencia/CompetenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likedinscrapping.competencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
